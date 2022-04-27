9adb4295088be35e619095795fe20c988229ba52
toreeulus <toreeolo@gmail.com>
2022-04-27 19:00:48 +0200

    Aggiunta classe Vibration nel Model

--- app/src/main/java/com/infinityloop/homepage/MainActivity.java
+++ app/src/main/java/com/infinityloop/homepage/MainActivity.java
@@ -32,9 +32,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
     public void displayToast(View view){ //messaggio toast indicante stato musica
-        if (count == 0) {
+        if (music.isPlaying()) {
             Toast.makeText(MainActivity.this, "Musica OFF", Toast.LENGTH_SHORT).show();
-        }else if(count == 1){
+        }else{
             Toast.makeText(MainActivity.this, "Musica ON", Toast.LENGTH_SHORT).show();
         }
     }
